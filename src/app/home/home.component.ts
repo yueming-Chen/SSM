9ac297e5ca865df3f38972ec26c29e58fd15e5d2
yueming-chen <aa1235561@gmail.com>
2017-07-10 00:21:47 +0800

    1.設定卡片為主頁內容

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-home',
-  templateUrl: './home.component.pug', 
-      styleUrls: ['./home.component.scss'] 
+  templateUrl: './home.component.pug',
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
 
