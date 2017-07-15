d2c58ebf5b51becaa6f73533d9f573aa3aa5830e
yueming-chen <aa1235561@gmail.com>
2017-07-16 00:03:40 +0800

    1.完成檔案預覽，刪除 2.預計完成拖移

--- src/app/create-score/create-score.component.ts
+++ src/app/create-score/create-score.component.ts
@@ -1,17 +1,47 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
+import { DomSanitizer } from '@angular/platform-browser';
 @Component({
   selector: 'app-create-score',
   templateUrl: './create-score.component.pug',
   styleUrls: ['./create-score.component.scss']
 })
 export class CreateScoreComponent implements OnInit {
+
   private borrowClick: boolean = false;
-  public scorename: string = "";
-  public items = ['a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a',];
-  constructor() { }
+  private cover = 'cover';
+  public scorename: string = '';
+  public scoreitems = [];
+  constructor(private domSanitizer: DomSanitizer) {
+
+  }
 
   ngOnInit() {
   }
 
+  uploadNewScore($event) {
+    this.fileread($event);
+  }
+
+  fileread(inputValue) {
+    let file: File[] = inputValue.target.files;
+    for (let index in file) {
+      let val = file[index];
+      if (file[index] instanceof File) {
+        let myReader: FileReader = new FileReader();
+        myReader.onloadend = function (e) {
+          this.scoreitems.push({ index: this.scoreitems.length + 1, url: this.domSanitizer.bypassSecurityTrustStyle('url(' + myReader.result + ')') });
+          console.log(this.scoreitems);
+        }.bind(this);
+        myReader.readAsDataURL(val);
+      }
+
+    }
+  }
+
+  remove(index) {
+    this.scoreitems = this.scoreitems
+      .filter(val => { return val.index !== index })
+      .map((val, index) => { val.index = index; return val });
+  }
 }
